7cfbc5cde42727cc3d524fb64f2df67526d5681f
Artem0222 <arzerby11@github.com>
2026-09-12 20:50:42 +0300

    stt repo

--- stats/stats-server/src/main/java/ru/practicum/main/stats/server/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/main/stats/server/repository/StatsRepository.java
@@ -3,19 +3,19 @@ package ru.practicum.main.stats.server.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.practicum.main.stats.server.model.Hit;
 import ru.practicum.main.stats.dto.ViewStats;
+import ru.practicum.main.stats.server.model.Hit;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsRepository extends JpaRepository<Hit, Long> {
 
-    @Query("""
-            SELECT new ru.practicum.stats.dto.ViewStats(h.app, h.uri, COUNT(h.ip))
-            FROM Hit h
+    @Query(nativeQuery = true, value = """
+            SELECT h.app AS app, h.uri AS uri, COUNT(h.ip) AS hits
+            FROM hits h
             WHERE h.timestamp BETWEEN :start AND :end
-            AND (:uris IS NULL OR h.uri IN :uris)
+            AND (:uris IS NULL OR h.uri IN (:uris))
             GROUP BY h.app, h.uri
             ORDER BY COUNT(h.ip) DESC
             """)
@@ -25,11 +25,11 @@ public interface StatsRepository extends JpaRepository<Hit, Long> {
             @Param("uris") List<String> uris
     );
 
-    @Query("""
-            SELECT new ru.practicum.stats.dto.ViewStats(h.app, h.uri, COUNT(DISTINCT h.ip))
-            FROM Hit h
+    @Query(nativeQuery = true, value = """
+            SELECT h.app AS app, h.uri AS uri, COUNT(DISTINCT h.ip) AS hits
+            FROM hits h
             WHERE h.timestamp BETWEEN :start AND :end
-            AND (:uris IS NULL OR h.uri IN :uris)
+            AND (:uris IS NULL OR h.uri IN (:uris))
             GROUP BY h.app, h.uri
             ORDER BY COUNT(DISTINCT h.ip) DESC
             """)
