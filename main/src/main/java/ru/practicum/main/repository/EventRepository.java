c71a8ffa68d26511148d5ec67aaea3c1f8fd52dd
Artem0222 <arzerby11@github.com>
2026-09-13 00:06:32 +0300

    event repository

--- main/src/main/java/ru/practicum/main/repository/EventRepository.java
+++ main/src/main/java/ru/practicum/main/repository/EventRepository.java
@@ -23,16 +23,16 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     boolean existsByCategoryId(Long categoryId);
 
     @Query("""
-            SELECT e FROM Event e
-            WHERE e.state = 'PUBLISHED'
-            AND (COALESCE(:text, NULL) IS NULL
-                OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%'))
-                OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%')))
-            AND (:categories IS NULL OR e.category.id IN :categories)
-            AND (:paid IS NULL OR e.paid = :paid)
-            AND e.eventDate >= :rangeStart
-            AND e.eventDate <= :rangeEnd
-            """)
+        SELECT e FROM Event e
+        WHERE e.state = 'PUBLISHED'
+        AND (COALESCE(:text, '') = ''
+            OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', CAST(:text AS string), '%'))
+            OR LOWER(e.description) LIKE LOWER(CONCAT('%', CAST(:text AS string), '%')))
+        AND (:categories IS NULL OR e.category.id IN :categories)
+        AND (:paid IS NULL OR e.paid = :paid)
+        AND e.eventDate >= :rangeStart
+        AND e.eventDate <= :rangeEnd
+        """)
     List<Event> findAllPublishedEvents(
             @Param("text") String text,
             @Param("categories") List<Long> categories,
